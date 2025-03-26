beb2a8342f91748c1db9cd7714557ee8c3a3ef00
Ben Johnson <ben.johnson4343@gmail.com>
2025-03-26 11:39:01 -0400

    roll back utils.R
    Modify transformation scores instead

--- R/utils.R
+++ R/utils.R
@@ -2650,7 +2650,7 @@ availableDefaultMapping <- function(file, trackType) {
         }
         ## scale the score to lwd.max
         # assume specific transformation for STORM paper
-        juns$scaled <- (lwd.max - 1) / (juns$score - 1) + lwd.max
+        juns$scaled <- (lwd.max - 1) / pmax((max(juns$score) - min(c(1, juns$score))), 1) * (juns$score - max(juns$score)) + lwd.max
         ## create list
         juns <- list(
             x = as.numeric(rbind(
